1c4b1c610a62b458fa08d8ba295e9127bd72a29e
Hai Duc Vu <duchaivu1997@gmail.com>
2023-01-12 00:24:36 +0700

    Working on shopping cart - 'userSchema.methods.addToCart'

--- models/user.js
+++ models/user.js
@@ -28,6 +28,40 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return (cp.productId?.toString() || '') === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+
+  // const updatedCart = {
+  //   items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+  // };
+  // const db = getDb();
+  // return db
+  //   .collection('users')
+  //   .updateOne(
+  //     { _id: new ObjectId(this._id) },
+  //     { $set: { cart: updatedCart } }
+  //   );
+  this.cart = updatedCart;
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');

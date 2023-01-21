59f8deeadc484904e2b77a0489f2fc193674eb90
Hai Duc Vu <duchaivu1997@gmail.com>
2023-01-21 14:49:53 +0700

    Fix some bug isAuthenticated

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-    isAuthenticated: req.isAuthenticated,
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
@@ -50,7 +50,7 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
-        isAuthenticated: req.isAuthenticated,
+        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
@@ -88,7 +88,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products',
-        isAuthenticated: req.isAuthenticated,
+        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));

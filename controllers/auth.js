03b063d447ad7262576a2a40c89780334429bf6a
Hai Duc Vu <duchaivu1997@gmail.com>
2023-01-28 16:35:37 +0700

    [Sending Email] - Using send gird service and nodemailer lib to sign up new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
 const bcrypt = require('bcryptjs');
 
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.API_KEY,
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -70,7 +82,7 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         req.flash(
           'error',
-          'Email exists already, please pick a different one.'
+          'E-Mail exists already, please pick a different one.'
         );
         return res.redirect('/signup');
       }
@@ -86,6 +98,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: process.env.EMAIL,
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>',
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
     })
     .catch((err) => {
@@ -97,5 +118,19 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect('/');
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message,
   });
 };

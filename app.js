067f5f0f41c67257e9aa472cc188826a9202065e
Hai Vu <duchaivu1997@gmail.com>
2023-06-12 23:59:27 +0700

    324: Filtering Files by MineTypes

--- app.js
+++ app.js
@@ -1,52 +1,64 @@
-require('dotenv').config();
-const path = require('path');
+require("dotenv").config();
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const session = require('express-session');
-const MongoDbStore = require('connect-mongodb-session')(session);
+const session = require("express-session");
+const MongoDbStore = require("connect-mongodb-session")(session);
 // using csurf to prevent CSRF Attacks
-const csrf = require('csurf');
-const flash = require('connect-flash');
-const multer = require('multer');
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const MONGODB_URI = `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.DATABASE_PASS}@cluster0.ir4appl.mongodb.net/shop?retryWrites=true&w=majority`;
 
 const app = express();
 const store = new MongoDbStore({
   uri: MONGODB_URI,
-  collection: 'sessions',
+  collection: "sessions",
 });
 
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mineType === "image/png" ||
+    file.mineType === "image/jpq" ||
+    file.mineType === "image/jeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({ storage: fileStorage, fileFilter }).single("image"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
-    secret: 'my secret',
+    secret: "my secret",
     resave: false, // is meaning the session will not be saved on every request that is done => performance
     saveUninitialized: false, // ensure that no session gets saved for a request where it doesn't need to be saved because nothing was changed about it
     store: store,
@@ -63,14 +75,14 @@ app.use((req, res, next) => {
     return next();
   }
   User.findById(req.session.user._id)
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return next();
       }
       req.user = user;
       next();
     })
-    .catch(err => {
+    .catch((err) => {
       next(new Error(err));
     });
 });
@@ -82,22 +94,22 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500', errorController.get500);
+app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...);
   // res.redirect('/500');
-  res.status(500).render('500', {
-    pageTitle: 'Error!',
-    path: '/500',
-    isAuthenticated: req.session.isLoggedIn
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
   });
 });
 
@@ -107,10 +119,10 @@ mongoose
     useUnifiedTopology: true,
   })
   .then((result) => {
-    console.log('DB connected');
+    console.log("DB connected");
     app.listen(3000);
   })
   .catch((err) => {
-    console.log('DB connect fail');
-    console.log('err', err);
+    console.log("DB connect fail");
+    console.log("err", err);
   });

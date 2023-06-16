911028c5265f004053e85c82a84eb3bf3d00cd9c
Hai Vu <duchaivu1997@gmail.com>
2023-06-16 17:12:55 +0700

    Serving Images Statically

--- app.js
+++ app.js
@@ -54,8 +54,10 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage, fileFilter }).single("image"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({

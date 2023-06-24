aaf9d3c4127b76ffeeafa6689b301701e0e3ae74
Hai Vu <duchaivu1997@gmail.com>
2023-06-24 20:56:56 +0700

    Generating .pdf Files with Order Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,7 +3,7 @@ const Order = require("../models/order");
 const User = require("../models/user");
 const fs = require("fs");
 const path = require("path");
-const PDFDocument = require('pdfkit');
+const PDFDocument = require("pdfkit");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -160,32 +160,40 @@ exports.getInvoice = (req, res, next) => {
       if (order.user.userId.toString() !== req.user._id.toString()) {
         return next(new Error("Unauthorized"));
       }
-      
+
       const invoiceName = "invoice-" + orderId + ".pdf";
       const invoicePath = path.join("data", "invoices", invoiceName);
       const pdfDoc = new PDFDocument();
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"');
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'attachment; filename="' + invoiceName + '"'
+      );
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
-      console.log('res', res)
+      console.log("res", res);
       pdfDoc.pipe(res);
-  
-      pdfDoc.text('Hello World!');
-  
+
+      // html
+
+      pdfDoc.fontSize(26).text("Invoice", { underline: true });
+      pdfDoc.text("------------------");
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice = totalPrice + prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              "-" +
+              prod.quantity +
+              "x" +
+              "$" +
+              prod.product.price
+          );
+      });
+      pdfDoc.text("------------------");
+      pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
       pdfDoc.end();
-      // fs.readFile(invoicePath, (err, data) => {
-      //   if (err) {
-      //     return next(err);
-      //   }
-      //   res.setHeader("Content-Type", "application/pdf");
-      //   res.setHeader(
-      //     "Content-Disposition",
-      //     'attachment; filename="' + invoiceName + '""'
-      //   ); // inline is open browser direct and attachment is download with file name
-      //   res.send(data);
-      // });
-      // const file = fs.createReadStream(invoicePath);
-      // file.pipe(res);
     })
     .catch((err) => next(err));
 };

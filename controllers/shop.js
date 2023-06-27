989ad03526844ca12c994340f26ed98dbce0dd14
Hai Vu <duchaivu1997@gmail.com>
2023-06-27 21:13:21 +0700

    Pagination logic

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,16 +5,41 @@ const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
 
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * parseInt(ITEMS_PER_PAGE))
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
-      // console.log(products);
+      // console.log('Object',{
+      //   pageTitle: 'Products',
+      //   path: '/products',
+      //   currentPage: page,
+      //   hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      //   hasPreviousPage: page > 1,
+      //   nextPage: page + 1,
+      //   previousPage: page - 1,
+      //   lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+      // })
       res.render("shop/product-list", {
         prods: products,
-        pageTitle: "All Products",
-        path: "/products",
+        pageTitle: 'Products',
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
@@ -42,26 +67,27 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1;
   let totalItems;
 
   Product.find()
     .countDocuments()
-    .then(numberProducts => {
+    .then((numberProducts) => {
       totalItems = numberProducts;
       return Product.find()
-      .skip((parseInt(page) - 1) * parseInt(ITEMS_PER_PAGE))
-      .limit(ITEMS_PER_PAGE)
+        .skip((page - 1) * parseInt(ITEMS_PER_PAGE))
+        .limit(ITEMS_PER_PAGE);
     })
     .then((products) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
         path: "/",
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         nextPage: page + 1,
         previousPage: page - 1,
+        hasPreviousPage: page > 1,
         lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })

9294c6ed29d713b9556c540eb13d9eae07030e7d
Hai Vu <duchaivu1997@gmail.com>
2023-06-30 00:17:42 +0700

    Sending & Handling BG Request

--- controllers/admin.js
+++ controllers/admin.js
@@ -190,8 +190,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
 
   Product.findById(prodId).then((product) => {
     if (!product) {
@@ -201,8 +201,11 @@ exports.postDeleteProduct = (req, res, next) => {
     Product.deleteOne({ _id: prodId, userId: req.user._id })
       .then(() => {
         console.log("DESTROYED PRODUCT");
-        res.redirect("/admin/products");
+        res.status(200).json({message: 'Success!'})
+        // res.redirect("/admin/products");
       })
-      .catch((err) => errorHandlerObjectWrapper(500, err));
+      .catch((err) => {
+        res.status(500).json({message: 'Deleting product failed.'})
+      });
   });
 };

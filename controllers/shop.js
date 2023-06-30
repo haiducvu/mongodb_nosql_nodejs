b3fa71e64747404056ffc1a47919ce84c7d230bf
Hai Vu <duchaivu1997@gmail.com>
2023-06-30 11:49:32 +0700

    Adding a Checkout Page

--- controllers/shop.js
+++ controllers/shop.js
@@ -239,3 +239,22 @@ exports.getInvoice = (req, res, next) => {
     })
     .catch((err) => next(err));
 };
+
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price
+      });
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total,
+      });
+    })
+    .catch((err) => errorHandlerObjectWrapper(500, err, next));
+};

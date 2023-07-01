5da5c24a3620ad79a148a7328fbca694a44f1a15
Hai Vu <duchaivu1997@gmail.com>
2023-07-01 21:42:45 +0700

    Using Stripe in App - Payment

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,7 @@ const User = require("../models/user");
 const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
 const ITEMS_PER_PAGE = 2;
 
@@ -240,21 +241,87 @@ exports.getInvoice = (req, res, next) => {
     .catch((err) => next(err));
 };
 
+/**
+  Data Test https://stripe.com/docs/testing?testing-method=card-numbers#cards
+  Card Information: 4242 4242 4242 4242
+  12/34 567
+ */
+
 exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
   req.user
-    .populate("cart.items.productId")
+    .populate('cart.items.productId')
     .then(user => {
-      const products = user.cart.items;
-      let total = 0;
+      products = user.cart.items;
+      total = 0;
       products.forEach(p => {
-        total += p.quantity * p.productId.price
+        total += p.quantity * p.productId.price;
       });
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map(p => {
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+              unit_amount: p.productId.price * 1,
+            },
+            quantity: p.quantity
+          };
+        }),
+        mode: 'payment',
+        success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+      });
+    })
+    .then(session => {
       res.render('shop/checkout', {
         path: '/checkout',
         pageTitle: 'Checkout',
         products: products,
         totalSum: total,
+        sessionId: session.id,
+        public_key: process.env.STRIPE_PUBLIC
       });
     })
-    .catch((err) => errorHandlerObjectWrapper(500, err, next));
+    .catch(err => {
+      const error = new Error(err);
+      console.log('Error Payment', error)
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
